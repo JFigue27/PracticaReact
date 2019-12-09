86c5cd45ab6bb21777899b47d282d0991427194b
JoseJaime Figueroa <josejaime.figueroa@molex.com>
2019-12-09 12:48:44 -0700

    - Agregando estilos e imagenes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../assets/styles/components/CarouselItem.scss';
 
+import play from '../assets/static/play-icon.png';
+import plus from '../assets/static/plus-icon.png';
+
 const CarouselItem = () => (
   <>
     <div className='carousel-item'>
@@ -11,8 +14,8 @@ const CarouselItem = () => (
       />
       <div className='carousel-item__details'>
         <div>
-          <img className='carousel-item__details--img' src='../assets/play-icon.png' alt='Play Icon' />
-          <img className='carousel-item__details--img' src='../assets/plus-icon.png' alt='Plus Icon' />
+          <img className='carousel-item__details--img' src={play} alt='Play Icon' />
+          <img className='carousel-item__details--img' src={plus} alt='Plus Icon' />
         </div>
         <p className='carousel-item__details--title'>Título descriptivo</p>
         <p className='carousel-item__details--subtitle'>2019 16+ 114 minutos</p>
